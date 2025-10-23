2af6ed7826c39b3ff642453d2e28d3a7f5c4ad5c
mouna-ben-mansour <benmansour.mouna2@gmail.com>
2025-10-23 15:12:19 +0200

    phpstan fix

--- Rectangle.php
+++ Rectangle.php
@@ -8,4 +8,13 @@ class Rectangle extends Shape
 {
     // Properties
     private $area = '20';
+
+    public function getArea()
+    {
+        return $this->area;
+    }
+    public function setArea($area)
+    {
+        $this->area = $area;
+    }
 }
